538ea232cc93d273786d6ab7ff3b976023ec0d5c
Oleksii Kyrylchuk <olety7@gmail.com>
2017-10-17 12:23:12 +0200

    Updated stock downloading
    - Changed the logging level on most log calls to debug
    - Made start and end dt into constants
    - Fixed another bugs with the tickers ('.'->'-')
    - Updated the stock getting loop

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import sys
 import time
 
 import bs4 as bs  # BeautifulSoup, HTML scraping
-import matplotlib.pyplot as plt  # Plots
+import matplotlib.pyplot as plt  # Plots q
 import numpy as np  # Arrays
 import pandas as pd  # DataFrames
 import pandas_datareader as web  # Gets stock data from Yahoo
@@ -32,7 +32,12 @@ logging.basicConfig(format='%(levelname)s | line %(lineno)s '
 # Numpy printing setup
 np.set_printoptions(threshold=10, linewidth=79, edgeitems=5)
 
-# %% Getting the tickers
+# Constants
+# We want to get the data from 2000 till 2017
+START_DT = dt.datetime(2000, 1, 1)
+END_DT = dt.datetime(2017, 1, 1)
+
+# %% Data gathering
 
 
 def get_snp_tickers():
@@ -44,18 +49,18 @@ def get_snp_tickers():
 
     # We'll be using wikipedia as a main data source
     URL = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    logging.info('Getting the wikipedia article...')
+    logging.debug('Getting the wikipedia article...')
     resp = requests.get(URL)
 
     # If we can't get the page, just throw an exception
-    logging.info('Checking the response code...')
+    logging.debug('Checking the response code...')
     if resp.status_code != 200:
         raise Exception('Couldn\'t access the url.'
                         'fResponse code {resp.status_code}')
     # We've gotten the page by now; start parsing the response
     soup = bs.BeautifulSoup(resp.text, 'html5lib')
     # Find the table containing the tickers
-    logging.info('Finding the table containing the tickers...')
+    logging.debug('Finding the table containing the tickers...')
     for table in soup.findAll('table', {'class': 'wikitable sortable'}):
         try:
             # We're searching for a table that has a 'Ticker symbol' as its
@@ -65,40 +70,44 @@ def get_snp_tickers():
         except Exception:
             pass
     # Start extracting the tickers from the table
-    logging.info('Extracting the tickers...')
+    logging.debug('Extracting the tickers...')
     res_arr = []
     for row in res_table.findAll('tr')[1:]:
         # Replacing dots with dashes because otherwise we won't be able to
         # download them from yahoo finance
-        res_arr.append(row.findAll('td')[0].a.contents[0].replace(',', '-'))
+        res_arr.append(
+            row.findAll('td')[0]
+            .a.contents[0]
+            .replace(',', '-').replace('.', '-')
+        )
     return res_arr
 
 
-def save_tickers(folder='data', fname='tickers.csv'):
+def save_tickers(folder='data/tickers', fname='tickers.csv'):
     '''
     Saves tickers got by get_snp_tickers into a file using Pickle.
 
     Throws an exception if something goes wrong.
     '''
-    logging.info('Getting the tickers...')
+    logging.debug('Getting the tickers...')
     tickers = pd.DataFrame(get_snp_tickers())
     if tickers.shape[0] != 505:
         raise Exception('Bad ticker count - '
                         '{} instead of 505'.format(len(tickers)))
-    logging.info('Gotten tickers')
+    logging.debug('Gotten tickers')
     tickers.columns = ['Ticker']
     display(tickers)
     # Sorting the tickers alphabetically
-    logging.info('Sorting the tickers...')
+    logging.debug('Sorting the tickers...')
     tickers.sort_values(by='Ticker', inplace=True)
-    logging.info('Tickers after sorting')
+    logging.debug('Tickers after sorting')
     display(tickers)
     # Saving the tickers
     fpath = os.path.join(os.getcwd(), folder, fname)
-    logging.info('Creating the destination folder...')
+    logging.debug('Creating the destination folder...')
     if not os.path.exists(os.path.dirname(fpath)):
         os.makedirs(os.path.dirname(fpath))
-    logging.info(f'Saving the tickers to {fpath}')
+    logging.debug(f'Saving the tickers to {fpath}')
 
     tickers.to_csv(fpath, index=False)
     logging.info(f'Saved the tickers to {fpath}')
@@ -106,60 +115,69 @@ def save_tickers(folder='data', fname='tickers.csv'):
     return tickers
 
 
-def get_stock_data(start_dt, end_dt, ticker_folder='data',
+def get_stock_data(start_dt, end_dt, ticker_folder='data/tickers',
                    ticker_fname='tickers.csv', reload_tickers=False,
-                   dest_folder='data', retry_requests=True, max_retries=50,
-                   timeout=2):
+                   dest_folder='data/stocks', retry_requests=True,
+                   max_retries=50, timeout=2):
     '''
     Gets stock data of S&P500 from yahoo and saves it in the {folder}/{tick}.csv
 
     Throws an exception is anything goes wrong.
     '''
-    logging.info('Obtaining the tickers...')
+    logging.debug('Obtaining the tickers...')
     if reload_tickers:
         tickers = save_tickers()
     else:
         tickers = pd.read_csv(os.path.join(os.getcwd(),
                                            ticker_folder, ticker_fname))
-    logging.info('Obtained the tickers...')
+    logging.debug('Obtained the tickers...')
     display(tickers)
     # We have to check whether the dest folder exists
     dest_path = os.path.join(os.getcwd(), dest_folder)
     if not os.path.exists(dest_path):
-        logging.info('Creating the destination folder')
+        logging.debug('Creating the destination folder')
         os.makedirs(dest_path)
 
     # Downloading the prices
-    logging.info('Starting processing tickers...')
+    logging.debug('Starting processing tickers...')
     for index, ticker in tqdm(tickers.itertuples(), desc='Tickers processed',
-                              leave=False, file=sys.stdout, unit='company',
+                              leave=False, file=sys.stderr, unit='company',
                               total=tickers.shape[0]):
+        logging.debug(f'Starting a new outer loop iteration for {ticker}')
         dest_fpath = os.path.join(dest_path, f'{ticker}.csv')
         if not os.path.exists(dest_fpath):
             # Try to download the stock for max_retries tries, waiting
             # for timeout in between tries
-            for i in tqdm(range(max_retries), desc='Number of tries',
-                          leave=False, file=sys.stderr, unit='try'):
+            pbar = tqdm(range(max_retries), desc='Number of tries',
+                        leave=False, file=sys.stderr, unit='try')
+            retries = max_retries
+            while retries > 0:
+                pbar.update(1)
+                retries -= 1
                 try:
-                    # TODO: Change all 3 to debug
-                    logging.info(f'Trying to get {ticker} data from yahoo...')
+                    logging.debug(f'Trying to get {ticker} data from yahoo...')
                     df = web.DataReader(ticker, 'yahoo', start_dt, end_dt)
-                    logging.info(f'Successfully got {ticker} data from yahoo. '
-                                 'Now saving it...')
-                    df.to_csv(dest_fpath)
-                    logging.info(f'Saved the {ticker} data.')
-                    break
                 except Exception:
-                    # TODO: Change to debug
-                    logging.info('Yahoo has denied our request - '
-                                 f'sleeping for {timeout} seconds')
+                    logging.debug('Yahoo has denied our request - '
+                                  f'sleeping for {timeout} seconds')
                     time.sleep(timeout)
+                logging.debug(f'Successfully got {ticker} data from yahoo. '
+                              'Now saving it...')
+                df.to_csv(dest_fpath)
+                logging.debug(f'Saved the {ticker} data.')
+                retries = 0
+            pbar.close()
+
         else:
-            logging.info(f'Not downloading data for {ticker}, '
-                         'since we already have it')
+            logging.debug(f'Not downloading data for {ticker}, '
+                          'since we already have it')
     logging.info('Finished processing all tickers!')
     logging.info(f'You can find the results in the folder {dest_path}')
 
 
-get_stock_data(start_dt=dt.datetime(2000, 1, 1),
-               end_dt=dt.datetime(2017, 1, 1))
+# save_tickers()
+get_stock_data(START_DT, END_DT)
+
+# %% Data visualization
+
+# %% Predictive analysis
